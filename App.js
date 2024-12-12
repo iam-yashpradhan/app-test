280b4b0f0cc9eaddfb4ece7cf342317929777f76
Yash Pradhan <iamyashpradhan@gmail.com>
2024-12-12 13:46:08 -0500

    changes in package versions

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      // <WebView
+      //     source={{
+      //       uri: 'http://127.0.0.1:3000/',
+      //     }}
+      //     originWhitelist={['https://*', 'bankid://*']}
+      //     onShouldStartLoadWithRequest={(r) => {
+      //       if (r.url.startsWith('bankid:///')) {
+      //         Linking.openURL(r.url)
+      //         return false
+      //       }
+      //       return true
+      //     }}
+      // />
+      <WebView
+          style={styles.container}
+          source={{ uri: 'https://cdibuild.netlify.app/' }}
+      />
+      // <Text style={styles.container}>
+      //   Yash Pradhan
+      // </Text>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
   },
 });
